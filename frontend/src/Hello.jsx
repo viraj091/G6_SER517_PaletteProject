11c3d9ad588b64d81ec8ee7b6dd375b21381a3d1
jake <jsmith181997@gmail.com>
2024-10-02 10:44:17 -0500

    initialize tailwind

---
+++ frontend/src/Hello.jsx
@@ -0,0 +1,9 @@
+export default function Hello() {
+    return (
+        <div className="h-screen flex flex-col items-center justify-center">
+            <h1 className="text-4xl font-bold text-violet-400">
+                Hi Kyler
+            </h1>
+        </div>
+    )
+}
